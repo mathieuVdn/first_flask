08908f3548c46e217ac0c3cbf96991aaed6e1722
mathieuVdn <mathieu.vaudon@gmail.com>
2024-01-05 20:07:57 +0100

    first commit

---
+++ app/routes.py
@@ -0,0 +1,29 @@
+from app import app
+from flask import render_template
+from app.forms import LoginForm
+
+@app.route('/')
+def homepage():
+    return "test"
+
+
+@app.route('/index')
+def index():
+    user = {"username": "Mathieu"}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)
