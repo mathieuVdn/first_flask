526b6b15f9df06ba20701ec727d04a9997da2f70
mathieuVdn <mathieu.vaudon@gmail.com>
2024-01-05 21:35:49 +0100

    auth done

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
+
+class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or 'tu-ne-sauras-jamais'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
 
